9f43e3f7961919d23203250e2d0b7cd6ee552e9a
grzesu1973 <grzesu1973@gmail.com>
2017-11-23 09:09:35 +0100

    pierwszy opis

---
+++ rk.c
@@ -0,0 +1,30 @@
+/* kompilacja: GNU C: gcc -Wall nazwa_pliku.c -lm */
+
+#include <stdio.h>
+#include <math.h>	/*biblioteka matematyczna dolanczana w kompilatorze -lm */
+
+void clear()
+{
+printf("\033[2J"); /* Czyści ekran */
+printf("\033[0;0f"); /* Ustawia kursor w lewym, górnym rogu */
+}
+
+void pobierz()
+	
+
+
+int main()
+{
+clear();
+	float a1, a2, b1, b2, c1 , c2; /* parametry rownania */
+	
+	printf("Program na układ równań.\n");
+	scanf("%f%f%f", &a, &b, &c);
+
+
+		\n");
+		printf(" x1 = %f\n x2 = %f\n", x1, x2);
+		}
+	}
+	return 0;
+}
